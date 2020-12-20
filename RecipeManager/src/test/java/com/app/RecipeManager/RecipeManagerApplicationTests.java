9839273a0a9aa74aba934307f4e78ae14d6e9220
Korosive <eddoman33433@gmail.com>
2020-12-20 15:24:18 +1100

    Base files

---
+++ RecipeManager/src/test/java/com/app/RecipeManager/RecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.RecipeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
